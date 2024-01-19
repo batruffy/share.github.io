7b97aa47089397430f54066d3e30a7d7bc6e1a6b
Dhruv Patel <batruffy@gmail.com>
2024-01-19 21:56:16 +0530

    jksbfgjhgf
    jdjbgihguht

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-let content = 'I Hate You😭'
+var content = 'I Hate You😭'
 
 let btn = document.querySelector('.share-btn')
 console.log(btn)
 
 btn.addEventListener('click', () => {
   for (let i = 0; i < 1000; i++) {
-    let whatsappUrl = 'whatsapp://send?text=' + encodeURIComponent(content)
-    window.location.href = whatsappUrl
+    content = content + content
   }
+  let whatsappUrl = 'whatsapp://send?text=' + encodeURIComponent(content)
+  window.location.href = whatsappUrl
 })
